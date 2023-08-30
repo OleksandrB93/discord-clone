4ee40f702655a557c046aaaf358623f0977e0aa7
OleksandrB93 <barabashsan4ik@gmail.com>
2023-08-30 22:48:25 +0300

    serverSidebar, invitations

--- app/(main)/(routes)/[serverId]/page.tsx
+++
@@ -1,5 +0,0 @@
-const ServerIdPage = () => {
-  return <div>serverIdpage</div>;
-};
-
-export default ServerIdPage;
--- components/navigation/NavigationAction.tsx
+++ components/navigation/NavigationAction.tsx
@@ -20,7 +20,7 @@ export const NavigationAction = () => {
           className="group flex items-center"
         >
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center
-         bg-zinc-200  shadow-lg dark:bg-neutral-700 group-hover:bg-emerald-500">
+         bg-white  shadow-lg dark:bg-neutral-700 group-hover:bg-emerald-500">
             <Plus
               className="group-hover:text-white transition text-emerald-500"
               size={25}
