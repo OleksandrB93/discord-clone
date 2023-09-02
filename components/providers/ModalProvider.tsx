2d7195fba17ace6608acd785de175e64ecd8a207
OleksandrB93 <barabashsan4ik@gmail.com>
2023-09-02 10:21:44 +0300

    searchChannel, ServerChannel list, EditChennels

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -5,9 +5,11 @@ import CreateServerModal from "@/components/modals/CreateServerModal";
 import InviteModal from "@/components/modals/InviteModal";
 import { EditServerModal } from "@/components/modals/EditServerModal";
 import { MembersModal } from "@/components/modals/MembersModal";
-import { CreateChannelModal } from "@/components/modals/CreateChanelModal";
+import { CreateChannelModal } from "@/components/modals/CreateChannelModal";
 import { LeaveServerModal } from "@/components/modals/LeaveServerModal";
 import { DeleteServerModal } from "@/components/modals/DeleteServerModal";
+import { DeleteChannelModal } from "@/components/modals/DeleteChennelModal";
+import { EditChannelModal } from "@/components/modals/EditChannelModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -29,8 +31,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
-      {/* <DeleteChannelModal /> */}
-      {/* <EditChannelModal /> */}
+      <DeleteChannelModal />
+      <EditChannelModal />
       {/* <MessageFileModal /> */}
       {/* <DeleteMessageModal /> */}
     </>
